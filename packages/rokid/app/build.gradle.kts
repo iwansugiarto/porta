b45352993ac7f5c7992f2209519202b7d56d8e9d
iwansugiarto <iwansugiarto@gmail.com>
2026-05-16 20:56:19 +0700

    chore(rokid): alpha release v0.3.7 (13)

--- packages/rokid/app/build.gradle.kts
+++ packages/rokid/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "id.infinia.porta"
         minSdk = 28
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.3.6"
+        versionCode = 13
+        versionName = "0.3.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
