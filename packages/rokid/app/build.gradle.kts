c180111d478463ba74dc16ef1c7cac8a86a5c307
iwansugiarto <iwansugiarto@gmail.com>
2026-05-21 11:09:13 +0700

    chore(rokid): internal release v0.4.38 (64)

--- packages/rokid/app/build.gradle.kts
+++ packages/rokid/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "id.infinia.porta"
         minSdk = 28
         targetSdk = 35
-        versionCode = 63
-        versionName = "0.4.37"
+        versionCode = 64
+        versionName = "0.4.38"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
